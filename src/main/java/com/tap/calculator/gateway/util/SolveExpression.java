85d6c68129afdd923ff16c83df367d724d0f5f03
merienxx <eugenioanna389@gmail.com>
2023-02-28 09:36:02 +0800

    Created dependency in calculator project to import packages  from this project.
    Used SOAP Webservices to get the result of mathematical expression.

---
+++ src/main/java/com/tap/calculator/gateway/util/SolveExpression.java
@@ -0,0 +1,65 @@
+package com.tap.calculator.gateway.util;
+
+import com.tap.calculator.gateway.util.soap.OperationsXml;
+
+public class SolveExpression {
+	private static String[] val;
+	private static String method;
+	private String result;
+	private String resOperation;
+	
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+	
+	public void solve(String expression) throws Exception {
+		try {
+			if (!(expression.isEmpty())){
+				if(expression.contains("+")) {
+					val = expression.split("\\+");
+					method = "Add";
+					resOperation = "AddResult";
+				}
+				else if (expression.contains("*")) {
+					val = expression.split("\\*");
+					method ="Multiply";
+					resOperation = "MultiplyResult";
+				}
+				else if (expression.contains("/")) {
+					val = expression.split("\\/");
+					method ="Divide";
+					resOperation = "DivideResult";
+				}
+				else if (expression.contains("-")) {
+					if (expression.startsWith("-")) {
+						expression = expression.substring(1);
+						val = expression.split("\\-");
+						val[0]="-"+val[0];
+					}
+					else {
+						val = expression.split("\\-");
+					}
+					method ="Subtract";
+					resOperation = "SubtractResult";
+				}
+				else {
+					setResult(expression);
+				}
+				
+				String soapEndpointUrl = "http://www.dneonline.com/calculator.asmx";
+		        String soapAction = "http://tempuri.org/"+ method;
+				result = OperationsXml.callSoapWebService(soapEndpointUrl, soapAction, method, val, resOperation);
+			}
+			else {
+				setResult(expression);
+			}			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}	 
+
+}
