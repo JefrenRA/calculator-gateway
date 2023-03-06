0eede09890dce3df482334f53c4e1c59ac10f0b2
merienxx <eugenioanna389@gmail.com>
2023-03-07 06:13:38 +0800

    Resolved unmarshal unexpected element error through input validation of negative sign in expression.

--- src/main/java/com/tap/calculator/gateway/ops/impl/ArithmeticOperations.java
+++ src/main/java/com/tap/calculator/gateway/ops/impl/ArithmeticOperations.java
@@ -18,21 +18,22 @@ public class ArithmeticOperations implements Operations {
 	private SOAPOperationsXml soapOperation = new SOAPOperationsXml();
 	private String soapEndpointUrl = "http://www.dneonline.com/calculator.asmx";
 	private String result;
-	
+
 	public String getResult() {
 		return result;
 	}
+
 	public void setResult(String result) {
 		this.result = result;
 	}
 
 	public String add(String[] addVal) {
 		try {
-			//Calls the SOAP WebService to do the addition
-	        String soapAction = "http://tempuri.org/Add";
-	        String operation = "Add";
+			// Calls the SOAP WebService to do the addition
+			String soapAction = "http://tempuri.org/Add";
+			String operation = "Add";
 			String response = soapOperation.callSoapWebService(soapEndpointUrl, soapAction, addVal, operation);
-
+			System.out.println(response);
 			result = unmarshalAdd(response);
 		} catch (JAXBException e) {
 			e.printStackTrace();
@@ -42,11 +43,11 @@ public class ArithmeticOperations implements Operations {
 
 	public String subt(String[] subtVal) {
 		try {
-			//Calls the SOAP WebService to do the subtraction
-	        String soapAction = "http://tempuri.org/Subtract";
-	        String operation = "Subtract";
+			// Calls the SOAP WebService to do the subtraction
+			String soapAction = "http://tempuri.org/Subtract";
+			String operation = "Subtract";
 			String response = soapOperation.callSoapWebService(soapEndpointUrl, soapAction, subtVal, operation);
-
+			System.out.println(response);
 			result = unmarshalSub(response);
 		} catch (JAXBException e) {
 			e.printStackTrace();
@@ -56,11 +57,11 @@ public class ArithmeticOperations implements Operations {
 
 	public String mul(String[] mulVal) {
 		try {
-			//Calls the SOAP WebService to do the multiplication
-	        String soapAction = "http://tempuri.org/Multiply";
-	        String operation = "Multiply";
+			// Calls the SOAP WebService to do the multiplication
+			String soapAction = "http://tempuri.org/Multiply";
+			String operation = "Multiply";
 			String response = soapOperation.callSoapWebService(soapEndpointUrl, soapAction, mulVal, operation);
-
+			System.out.println(response);
 			result = unmarshalMul(response);
 		} catch (JAXBException e) {
 			e.printStackTrace();
@@ -70,61 +71,53 @@ public class ArithmeticOperations implements Operations {
 
 	public String div(String[] divVal) {
 		try {
-			//Calls the SOAP WebService to do the division
-	        String soapAction = "http://tempuri.org/Divide";
-	        String operation = "Divide";
+			// Calls the SOAP WebService to do the division
+			String soapAction = "http://tempuri.org/Divide";
+			String operation = "Divide";
 			String response = soapOperation.callSoapWebService(soapEndpointUrl, soapAction, divVal, operation);
-
+			System.out.println(response);
 			result = unmarshalDiv(response);
 		} catch (JAXBException e) {
 			e.printStackTrace();
 		}
 		return result;
 	}
-	
-	public void solveExpression(String expression) throws Exception {	
-		//Classifies the arithmetic operation used and store the given numbers to array
+
+	public void solveExpression(String expression) throws Exception {
+		// Classifies the arithmetic operation used and store the given numbers to array
 		try {
-			String[] val= {};
-			
-			if (!(expression.isEmpty())){
-				if(expression.contains("+")) {
+			String[] val = {};
+			if (!(expression.isEmpty())) {
+				if (expression.contains("+")) {
 					val = expression.split("\\+");
 					this.add(val);
-				}
-				else if (expression.contains("*")) {
+				} else if (expression.contains("*")) {
 					val = expression.split("\\*");
 					this.mul(val);
-				}
-				else if (expression.contains("/")) {
+				} else if (expression.contains("/")) {
 					val = expression.split("\\/");
 					this.div(val);
-				}
-				else if (expression.contains("-")) {
+				} else if (expression.contains("-")) {
 					if (expression.startsWith("-")) {
 						expression = expression.substring(1);
 						val = expression.split("\\-");
-						val[0]="-"+val[0];
-					}
-					else {
+						val[0] = "-" + val[0];
+					} else {
 						val = expression.split("\\-");
 					}
 					this.subt(val);
-				}
-				else {
+				} else {
 					setResult(expression);
 				}
-			}
-			else {
+			} else {
 				setResult(expression);
-			}			
-		}
-		catch(Exception e) {
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	//Unmarshal xml string to get the sum
+
+	// Unmarshal xml string to get the sum
 	public String unmarshalAdd(String xmlString) throws JAXBException {
 		JAXBContext jaxbContext = JAXBContext.newInstance(AddResponse.class);
 		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
@@ -133,8 +126,8 @@ public class ArithmeticOperations implements Operations {
 		AddResponse add = (AddResponse) unmarshaller.unmarshal(reader);
 		return String.valueOf(add.getAddResult());
 	}
-	
-	//Unmarshal xml string to get the difference
+
+	// Unmarshal xml string to get the difference
 	public String unmarshalSub(String xmlString) throws JAXBException {
 		JAXBContext jaxbContext = JAXBContext.newInstance(SubtractResponse.class);
 		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
@@ -143,8 +136,8 @@ public class ArithmeticOperations implements Operations {
 		SubtractResponse sub = (SubtractResponse) unmarshaller.unmarshal(reader);
 		return String.valueOf(sub.getSubResult());
 	}
-	
-	//Unmarshal xml string to get the product
+
+	// Unmarshal xml string to get the product
 	public String unmarshalMul(String xmlString) throws JAXBException {
 		JAXBContext jaxbContext = JAXBContext.newInstance(MultiplyResponse.class);
 		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
@@ -153,8 +146,8 @@ public class ArithmeticOperations implements Operations {
 		MultiplyResponse mul = (MultiplyResponse) unmarshaller.unmarshal(reader);
 		return String.valueOf(mul.getMulResult());
 	}
-	
-	//Unmarshal xml string to get the quotient
+
+	// Unmarshal xml string to get the quotient
 	public String unmarshalDiv(String xmlString) throws JAXBException {
 		JAXBContext jaxbContext = JAXBContext.newInstance(DivideResponse.class);
 		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
--- src/main/java/com/tap/calculator/gateway/soap/util/SOAPOperationsXml.java
+++ src/main/java/com/tap/calculator/gateway/soap/util/SOAPOperationsXml.java
@@ -13,61 +13,63 @@ import javax.xml.soap.SOAPPart;
 
 @SuppressWarnings("restriction")
 public class SOAPOperationsXml {
-    private String response;
-	
-	public  String callSoapWebService(String soapEndpointUrl, String soapAction, String[] val, String operation) {
-        try {
-        	XmlBuilder xmlBuild = new XmlBuilder();
-        	
-            // Create SOAP Connection
-            SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
-            SOAPConnection soapConnection = soapConnectionFactory.createConnection();
+	private String response;
 
-            // Send SOAP Message to SOAP Server
-            SOAPMessage soapResponse = soapConnection.call(createSOAPRequest(soapAction, val, operation), soapEndpointUrl);
-            
-            //Converts xml response to string
-            xmlBuild.parseSoapMessageBody(soapResponse);
-            response=xmlBuild.toXMLString();
-            
-            soapConnection.close();
-            
-        } catch (Exception e) {}
-        
+	public String callSoapWebService(String soapEndpointUrl, String soapAction, String[] val, String operation) {
+		try {
+			XmlBuilder xmlBuild = new XmlBuilder();
+
+			// Create SOAP Connection
+			SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
+			SOAPConnection soapConnection = soapConnectionFactory.createConnection();
+
+			// Send SOAP Message to SOAP Server
+			SOAPMessage soapResponse = soapConnection.call(createSOAPRequest(soapAction, val, operation),
+					soapEndpointUrl);
+
+			// Converts xml response to string
+			xmlBuild.parseSoapMessageBody(soapResponse);
+			response = xmlBuild.toXMLString();
+
+			soapConnection.close();
+
+		} catch (Exception e) {
+		}
+
 		return response;
-    }
-	
-	//Creates SOAP Request and get the SOAP Message
+	}
+
+	// Creates SOAP Request and get the SOAP Message
 	private SOAPMessage createSOAPRequest(String soapAction, String[] val, String operation) throws Exception {
-        MessageFactory messageFactory = MessageFactory.newInstance();
-        SOAPMessage soapMessage = messageFactory.createMessage();
-        SOAPPart soapPart = soapMessage.getSOAPPart();
-        SOAPEnvelope envelope = soapPart.getEnvelope();
-        String nameSpace = "Calculator";
-        String nameSpaceURI = "http://tempuri.org/";
-        
-        envelope.addNamespaceDeclaration(nameSpace, nameSpaceURI);
-        createSoapOperateEnvelope(envelope, val, operation);
+		MessageFactory messageFactory = MessageFactory.newInstance();
+		SOAPMessage soapMessage = messageFactory.createMessage();
+		SOAPPart soapPart = soapMessage.getSOAPPart();
+		SOAPEnvelope envelope = soapPart.getEnvelope();
+		String nameSpace = "Calculator";
+		String nameSpaceURI = "http://tempuri.org/";
 
-        MimeHeaders headers = soapMessage.getMimeHeaders();
-        headers.addHeader("SOAPAction", soapAction);
+		envelope.addNamespaceDeclaration(nameSpace, nameSpaceURI);
+		createSoapOperateEnvelope(envelope, val, operation, nameSpace);
 
-        soapMessage.saveChanges();
+		MimeHeaders headers = soapMessage.getMimeHeaders();
+		headers.addHeader("SOAPAction", soapAction);
 
-        return soapMessage;
-    }
-	
-	//Throw the operation used and the integer values to the element in the SOAP WebService and creates an SOAP Envelope
-	private void createSoapOperateEnvelope(SOAPEnvelope envelope, String[] val, String operation) throws SOAPException {
-       try {
-    	   String nameSpace = "Calculator";
-    	   SOAPBody soapAddBody = envelope.getBody();
-           SOAPElement soapAddBodyElem = soapAddBody.addChildElement(operation, nameSpace);
-           SOAPElement soapAddSubElem1 = soapAddBodyElem.addChildElement("intA", nameSpace);
-           SOAPElement soapAddSubElem2 = soapAddBodyElem.addChildElement("intB", nameSpace);
-           soapAddSubElem1.addTextNode(val[0]);
-           soapAddSubElem2.addTextNode(val[1]);
-       }
-       catch(Exception e) {}
-    }
+		soapMessage.saveChanges();
+
+		return soapMessage;
+	}
+
+	// Throw the operation used and the integer values to the element in the SOAP
+	// WebService and creates an SOAP Envelope
+	private void createSoapOperateEnvelope(SOAPEnvelope envelope, String[] val, String operation, String nameSpace) throws SOAPException {
+		try {
+			SOAPBody soapAddBody = envelope.getBody();
+			SOAPElement soapAddBodyElem = soapAddBody.addChildElement(operation, nameSpace);
+			SOAPElement soapAddSubElem1 = soapAddBodyElem.addChildElement("intA", nameSpace);
+			SOAPElement soapAddSubElem2 = soapAddBodyElem.addChildElement("intB", nameSpace);
+			soapAddSubElem1.addTextNode(val[0]);
+			soapAddSubElem2.addTextNode(val[1]);
+		} catch (Exception e) {
+		}
+	}
 }
